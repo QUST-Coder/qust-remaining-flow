d28f4beac86c4f4bd9b275a826a1022ad7d2a75d
Evan Mu <190567268@qq.com>
2016-05-28 21:39:52 +0800

    更新superAgent和cheerio的版本

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var superagent = require('superagent');
 var cheerio = require('cheerio');
 
-superagent.get('http://dr.com')
-    .end(function (err, sres) {
-        var $ = cheerio.load(sres.text);
-        var used_flux = $("script").text();
-        var start = used_flux.indexOf("flow='") + 6;
-        var end = start + 7;
-        used_flux = parseInt(used_flux.slice(start, end))/1024;
-        remaining = parseInt(1024 * 8 -used_flux);
-        console.log(remaining + ' Mb');
+superagent.get('http://172.16.10.3')
+          .end(function (err, res) {
+              var $ = cheerio.load(res.text);
+              var used_flux = $("script").text();
+              var start = used_flux.indexOf("flow='") + 6;
+              var end = start + 7;
+              used_flux = parseInt(used_flux.slice(start, end))/1024;
+              remaining = parseInt(1024 * 8 -used_flux);
+              console.log(remaining + ' Mb');
     });
